d3484bb977cb9e973d8ae0a7976927dd44f181dc
SergeyShevtsov <sergeyprostoas@gmail.com>
2025-06-15 18:34:46 +0300

    Fix issues

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,6 @@
 import { themes } from 'storybook/theming';
 import type { Preview } from '@storybook/react-vite';
-import '@torr-app/styles';
+import '@torr-app/styles/styles.css';
 
 const preview: Preview = {
   parameters: {
