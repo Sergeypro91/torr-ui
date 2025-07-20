7350ab7c7bfa7325b6a22425df0312fd90541e00
SergeyShevtsov <sergeyprostoas@gmail.com>
2025-07-20 23:16:54 +0300

    feat(semver): rebuild config
    Change main release.config.cjs

--- release.config.cjs
+++ release.config.cjs
@@ -1,28 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-
-const packagesDir = path.join(__dirname, 'packages');
-
 module.exports = {
   branches: ['main'],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json'],
-        message:
-          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
+  plugins: [],
+  extends: [
+    './packages/components/release.config.cjs',
+    './packages/icons/release.config.cjs',
+    './packages/styles/release.config.cjs',
+    './packages/utils/release.config.cjs',
   ],
-  // Release each package independently
-  extends: Object.entries(fs.readdirSync(packagesDir))
-    .filter(([pkg]) =>
-      fs.existsSync(path.join(packagesDir, pkg, 'package.json')),
-    )
-    .map(([pkg]) => `./packages/${pkg}/release.config.cjs`),
 };
