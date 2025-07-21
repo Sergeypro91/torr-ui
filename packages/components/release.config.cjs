798133fc0942c7b1eb345e403f634b834d204b98
SergeyShevtsov <sergeyprostoas@gmail.com>
2025-07-22 01:29:18 +0300

    feat(semver): setup multi semver
    Setup multi semantic-versioning

--- packages/components/release.config.cjs
+++ packages/components/release.config.cjs
@@ -2,7 +2,6 @@ const baseConfig = require('../../release.base.config.cjs');
 
 module.exports = {
   ...baseConfig,
-  pkgRoot: 'dist',
   tagFormat: 'components-v${version}',
   plugins: [
     ...baseConfig.plugins,
@@ -11,7 +10,7 @@ module.exports = {
       {
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-        assets: ['packages/components/CHANGELOG.md'],
+        assets: ['packages/utils/CHANGELOG.md', 'packages/utils/package.json'],
       },
     ],
   ],
